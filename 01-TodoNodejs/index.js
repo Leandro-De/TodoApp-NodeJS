aad8bd180bc102792af8fc8cfc088e666d927dc4
Yesid Varela <yleandrovarela@gmail.com>
2021-09-28 17:20:54 -0500

    Logia de las tareas por hacer

--- 01-TodoNodejs/index.js
+++ 01-TodoNodejs/index.js
@@ -1,5 +1,6 @@
 require("colors");
 const { inquirerMenu, pausa } = require("./helpers/inquirer");
+const Tarea = require("./models/tarea");
 
 console.clear();
 
@@ -7,8 +8,11 @@ const main = async () => {
   let opt = "";
 
   do {
-    opt = await inquirerMenu();
-    console.log({ opt });
+    // opt = await inquirerMenu();
+    // console.log({ opt });
+    const tarea = new Tarea("Comer");
+    console.log(tarea);
+
     await pausa();
   } while (opt != "0");
 };
